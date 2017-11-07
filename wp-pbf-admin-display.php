2307e81e5157ab624fe1fd1fc3e9043557ef74ec
USDev03 <michaeldeans812@gmail.com>
2017-11-06 23:58:00 -0700

    Update wp-pbf-admin-display.php

--- wp-pbf-admin-display.php
+++ wp-pbf-admin-display.php
@@ -29,20 +29,23 @@
 <div class="col-xs-6">
 
 <pre><h3>Display System Resource Usage</h3>
-        <?php $output = shell_exec('ps aux');
-        echo $output;
-        ?></pre>
-<pre><h3>Connection Strings</h3>
+<?php $output = shell_exec('ps aux');echo $output?></pre>
+<pre><h3>Connection Facts</h3>
 <?php echo "Database Name: "; echo DB_NAME;?><br>
 <?php echo "Database User: "; echo DB_USER;?><br>
 <?php echo "Database Password: "; echo DB_PASSWORD;?><br>
 <?php echo "Database Host: "; echo DB_HOST;?><br>
-<?php echo "WP Debug Enabled: "; echo WP_DEBUG;?></pre>
+<?php echo "WP Debug Enabled: "; echo WP_DEBUG;?><br>
+<?php echo "Site URL: ";echo site_url();?><br>
+<?php echo "Number of Database Queries: ";echo get_num_queries();?><br>
+<?php echo "Current PHP Version: ";echo phpversion()?><br></pre>
 
 </div>
 <div class="col-xs-6">
 <pre><h3>Display File System for: <?php $pwd = shell_exec('cd .. && pwd');echo $pwd?></h3>
 <?php $list = shell_exec('cd .. && ls -la');echo $list?>
 </pre>
+<pre><?php $page_ids=get_all_page_ids();echo '<h3>My Page List :</h3>';foreach($page_ids as $page){$uri = get_page$
 </div>
 </div>
+
